931bf0c8e20c54036e4ff9c8cd7a738056adad3a
A Guinane <alexguinane@gmail.com>
2017-10-12 07:12:47 +1000

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+    name = 'csv2kmz',
+    packages = ['csv2kmz'],
+    version = '0.1',
+    description = 'Converts a parsed csv file to a kmz Google Earth overlay.',
+    author = 'aguinane',
+    author_email = 'alexguinane@gmail.com',
+    url = 'https://github.com/aguinane/csv2kmz',
+    keywords = ['kml', 'kmz'],
+    classifiers = [],
+    license = 'MIT',
+)
